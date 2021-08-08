a78ff9a9b2d3b33a290732e9d55a35c3c5a797b0
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-08-08 15:19:13 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,4 +1,6 @@
 const client = require("../index");
+const antilinkData = require("../models/antilink.js");
+const prefixModel = require("../models/prefix.js");
 
 client.on("messageCreate", async (message) => {
   if (
@@ -7,9 +9,45 @@ client.on("messageCreate", async (message) => {
     !message.content.toLowerCase().startsWith(client.config.prefix)
   )
     return;
-
+  //anti link
+  const antilink = await antilinkData.findOne({
+    GuildID: message.guild.id,
+  });
+  if (antilink) {
+    if (
+      message.content.match("https://") ||
+      message.content.match("discord.gg") ||
+      message.content.match("www.")
+    ) {
+      message.delete();
+      message.channel
+        .send("No links allowed while anti-link is active!")
+        .then((msg) => {
+          let time = "2s";
+          setTimeout(function () {
+            msg.delete();
+          }, ms(time));
+        });
+    }
+  }
+  //custom prefix
+  const cprefix = await prefixModel.findOne(
+    {
+      GuildID: message.guild.id,
+    },
+    (error) => {
+      if (error) {
+        console.log(error);
+      }
+    }
+  );
+  
+  let PREFIX = cprefix ? cprefix.Prefix : client.config.prefix;
+   if (!message.guild.me.permissionsIn(message.channel).has("SEND_MESSAGES"))
+    return;
+  //cmd run
   const [cmd, ...args] = message.content
-    .slice(client.config.prefix.length)
+    .slice(PREFIX.length)
     .trim()
     .split(" ");
 
