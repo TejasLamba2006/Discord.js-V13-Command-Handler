455336ae52ef666da143041e81f42b13685ddc93
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-08-08 14:35:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const { Client, Collection } = require("discord.js");
 
 const client = new Client({
+  messageCacheLifetime: 60,
+  fetchAllMembers: false,
+  messageCacheMaxSize: 10,
+  restTimeOffset: 0,
+  restWsBridgetimeout: 100,
+  allowedMentions: {
+    parse: ["roles", "users", "everyone"],
+    repliedUser: true,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
   intents: [
     "GUILDS",
-    "GUILD_MEMBERS",
-    "GUILD_BANS",
-    "GUILD_EMOJIS",
-    "GUILD_MESSAGE_REACTIONS",
-    "GUILD_MESSAGES",
-  ],
+    "GUILD_BANS", "GUILD_VOICE_STATES", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
 });
 module.exports = client;
 client.commands = new Collection();
